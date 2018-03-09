c5fb90be1db477c1a14711d4b5cc20ebf2aad6ed
dagudelo30 <diegoagudelo30@gmail.com>
2018-03-09 10:01:15 -0500

    The function was adjusted to predict 1,2 and 3 months.

--- download_ersst.R
+++ download_ersst.R
@@ -4,33 +4,29 @@
 #Created by: Lizeth Llanos and Diego Agudelo
 #Date: February 2018
 
-
-if(require(stringr)==FALSE){install.packages("stringr",dependencies = TRUE)}
-library("stringr")
-
-download_ERSST_CPT=function(firs_year,last_year,i_month,l_season,dir_save,m_for,area1){
+download_ERSST_CPT=function(firs_year,last_year,i_month,l_season,dir_save,m_for,l_for,area1){
   
   trimestrel <- i_month:(i_month+l_season-1)
- 
-   if(sum(trimestrel>12)>0)trimestrel[which(trimestrel>12)]=trimestrel[which(trimestrel>12)]-12
+  fores <- m_for:(m_for+l_for-1)
+  if(sum(fores>12)>0)fores[which(fores>12)]=fores[which(fores>12)]-12
+  if(sum(trimestrel>12)>0)trimestrel[which(trimestrel>12)]=trimestrel[which(trimestrel>12)]-12
   route <- paste0("http://iridl.ldeo.columbia.edu/SOURCES/.NOAA/.NCDC/.ERSST/.version4/.sst/T/%28", month.abb[trimestrel[1]] ,"%20", firs_year ,"%29%28",  month.abb[trimestrel[l_season]] ,"%20", last_year ,"%29RANGEEDGES/T/", l_season ,"/boxAverage/T/12/STEP/Y/%28",area1[4],"%29%28",area1[3],"%29RANGEEDGES/X/%28",area1[1],"%29%28",area1[2],"%29RANGEEDGES/-999/setmissing_value/Y/high/low/RANGE/%5BX/Y%5D%5BT%5Dcptv10.tsv.gz")
-  m_for_final=str_pad(m_for, 2, pad = "0")
-  path_save <- paste0(dir_save,"/",m_for_final,"_",paste(month.abb[trimestrel],collapse = "-"),".tsv.gz")
+  path_save <- paste0(dir_save,"/",paste(month.abb[fores],collapse = "-"),"_",paste(month.abb[trimestrel],collapse = "-"),".tsv.gz")
   download.file(route,path_save)
- 
-   return("Successful download")
+  
+  return("Successful download")
   
 }
-
 
 #Example to download January from ERSST v4.0 
 
 area1 <- c(0,359,-30,30) #xmin, xmax, ymin, ymax
-i_month <- 1 #First month to download
-l_season <- 1 #Length season (1, 2, 3. meses)
+i_month <- 2 #First month to download
+l_season <- 3 #Length season (1, 2, 3. meses)
 m_for <- 4 #Month to forecast
+l_for <- 3 #Length forescast 
 firs_year <- 1981 #Initial year
 last_year <- 2018 #Last year
 dir_save <- "C:/Users/dagudelo/Desktop/ERSST" #Path 
 
-download_ERSST_CPT(firs_year,last_year,i_month ,l_season ,dir_save,m_for ,area1)
+download_ERSST_CPT(firs_year,last_year,i_month ,l_season ,dir_save,m_for,l_for,area1)
