b264d5637cfbfc8d0a03cf0cd0c90efab85a9641
Llanos Herrera <L.Llanos@cgiar.org>
2018-11-27 09:00:48 -0500

    add scree plots

--- result_maps.R
+++ result_maps.R
@@ -23,6 +23,8 @@ cca_map <- function(path_raw , path_output) {
   xloadcca <-  read.csv(paste0(path_raw, "_cca_load_x.txt"),skip =2, header=T, sep="",check.names = F)
   yloadcca <-  read.csv(paste0(path_raw, "_cca_load_y.txt"),skip =2, header=T, sep="")
   
+  xeigen<- read.csv(paste0(path_raw, "_pca_eigen_x.txt"),skip =2, header=T, sep="")
+  yeigen <- read.csv(paste0(path_raw,"_pca_eigen_y.txt"),skip =2, header=T, sep="")
   
   
   ## =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= ##
@@ -46,12 +48,8 @@ cca_map <- function(path_raw , path_output) {
   Map_x<- rasterVis::gplot(mapa_base) + geom_tile(aes(fill = value)) + coord_equal() + 
     labs(title="X Spatial Loadings (Mode 1)",x="",y=" ", fill = " ")  + theme(legend.key.height=unit(0.5,"cm"),legend.key.width=unit(2,"cm"),
                                                                               legend.text=element_text(size=10),
-                                                                              panel.background=element_rect(fill="white",colour="black"),
-                                                                              axis.text=element_text(colour="black",size=12),
-                                                                              axis.title=element_text(colour="black",size=12,face="bold"),
-                                                                              legend.position = "bottom", 
                                                                               legend.title = element_text(size = 12.5))  +
-    scale_fill_gradientn(colours =myPalette(100), limits=c(-1,1))   
+                                                                        scale_fill_gradientn(colours =myPalette(100), limits=c(-1,1))   
   
   yloadcca$val = yloadcca$X1/max(abs(yloadcca$X1),na.rm=T)
   y = yloadcca
@@ -62,8 +60,6 @@ cca_map <- function(path_raw , path_output) {
   sel <<-new_map[new_map$lat>ext_y[1] & new_map$lat<ext_y[2] & new_map$long>ext_y[3] & new_map$long<ext_y[4],]
   
    
-  # Realice el gráfico de las correlaciones entre las estaciones y el modo 1 de y 
-  # IT <- getData(name = "GADM", country = "Honduras", level = 1)
   p <- ggplot(sel, aes(x=long,y=lat)) # gráfique el pa�???s
   p <- p + geom_polygon(aes(fill=hole,group=group),fill="snow") + 
     scale_fill_manual(values=c("grey 80","grey 80"))  
@@ -93,13 +89,18 @@ cca_map <- function(path_raw , path_output) {
   # quite los valore NA
   datos[,1:2]=datos[,1:2]*100 # multipliquelos * 100
   
+  
+  datos_e <- data.frame(modes=xeigen$Mode, eigenx=xeigen$variance, eigeny=yeigen$variance, row.names = NULL)
+  
   # gráfico de los modos 
-  modos <-  ggplot(datos, aes(date,group = 1)) +   geom_line(aes(y = X ),  colour="firebrick3" ) + 
-    geom_line(aes(y = Y),  colour="forestgreen")  + 
-    geom_hline(yintercept = 0, colour="gray") + theme_bw() + 
-    theme( title =element_text(size=12, face='bold'),axis.text.y = element_text(size=12),  legend.position = "none", axis.text.x = element_text(angle = 90, hjust = 1, size = 12)) +
-    guides(colour = guide_legend(title = " ")) + labs(subtitle=paste("Canonical Correlation = ", round(cor(datos$X,datos$Y),3),sep = ""),x="",y="Scores (X red; Y green) (*100)",
-                                                      title = "Temporal Scores (Mode 1)") + scale_x_continuous(breaks = seq(min(datos$date),max(datos$date),3))
+  modosx <-  ggplot(datos_e, aes(modes,group = 1)) +   geom_line(aes(y = eigenx ),  colour="firebrick3" ) + geom_point(aes(y = eigenx ),  colour="firebrick3" ) +
+     theme_bw() + theme( title =element_text(size=12, face='bold'),axis.text.y = element_text(size=12),  legend.position = "none", axis.text.x = element_text(angle = 0, hjust = 1, size = 12)) +
+    guides(colour = guide_legend(title = " ")) + labs(x="Mode",y="% variance",title = "X Scree Plot") 
+  
+  
+  modosy <-  ggplot(datos_e, aes(modes,group = 1)) +   geom_line(aes(y = eigeny ),  colour="firebrick3" ) + geom_point(aes(y = eigeny ),  colour="firebrick3" ) +
+    theme_bw() + theme( title =element_text(size=12, face='bold'),axis.text.y = element_text(size=12),  legend.position = "none", axis.text.x = element_text(angle = 0, hjust = 1, size = 12)) +
+    guides(colour = guide_legend(title = " ")) + labs(x="Mode",y="% variance",title = "Y Scree Plot") 
   
   layt<-grid.layout(nrow=1,ncol=3,widths=c(4/9,2.5/9, 2.5/9),default.units=c('null','null'))
  
@@ -110,8 +111,18 @@ cca_map <- function(path_raw , path_output) {
   print(modos,vp=viewport(layout.pos.row=1,layout.pos.col=2))
   print(p,vp=viewport(layout.pos.row=1,layout.pos.col=3))
   dev.off()
-  cat("Mapas CCA realizados...")
+  cat("Mapas CCA realizados...\n")
   
+  layt<-grid.layout(nrow=1,ncol=2)
+  
+  tiff(filename = paste0(path_output, "/eigen_plot.tif"), width = 1500, height = 800,res=150,compression = 'lzw')
+  grid.newpage()
+  pushViewport(viewport(layout=layt))
+  print(modosx,vp=viewport(layout.pos.row=1,layout.pos.col=1))
+  print(modosy,vp=viewport(layout.pos.row=1,layout.pos.col=2))
+  dev.off()
+  cat("Scree plots realizados...\n")
+
 }
 
 cca_map(path_raw,path_output)
@@ -125,6 +136,8 @@ metric_map <- function(path_metric, path_output){
   trimesters <- unique(all_ind_complete$file)
   
   for(i in 1:length(trimesters)){
+    
+    
     
     all_ind <- all_ind_complete[all_ind_complete$file==trimesters[i],]
     
@@ -198,7 +211,7 @@ metric_map <- function(path_metric, path_output){
     print(ind_3,vp=viewport(layout.pos.row=2,layout.pos.col=1))
     
     dev.off()
-    cat("Mapas ROC realizados...")
+    cat("Mapas ROC realizados...\n")
     
     layt<-grid.layout(nrow=2,ncol=1)
     
@@ -209,7 +222,7 @@ metric_map <- function(path_metric, path_output){
     print(ind_5,vp=viewport(layout.pos.row=2,layout.pos.col=1))
     
     dev.off()
-    cat("Mapas Hit realizados...")
+    cat("Mapas Hit realizados...\n")
     
     max_C<-apply(all_ind[,10:12], 1, max)
     cat<-ifelse(all_ind[,10]==max_C, "Below", ifelse(all_ind[,11]==max_C, "Normal", ifelse(all_ind[,12]==max_C, "Above",0)))
@@ -240,7 +253,7 @@ metric_map <- function(path_metric, path_output){
     tiff(paste0(path_output,"/" ,trimesters[i], "_probabilistic_maps.tif"),width = 3000, height = 3000, units = "px", res = 400,compression = 'lzw')
     print(maxi)
     dev.off()  
-    cat("Mapas Probabilistico realizados...")
+    cat("Mapas Probabilistico realizados...\n")
     
     
     myPalette2 = colorRampPalette(c("steelblue2" , "deepskyblue", "lightcyan","khaki","yellow", "orange1","darkorange3", "red", "firebrick4"))
@@ -248,21 +261,21 @@ metric_map <- function(path_metric, path_output){
     above <-ggplot(sel, aes(x=long,y=lat)) + 
       geom_polygon(aes(fill=hole,group=group),fill="snow") +  
       geom_point(data=all_ind, aes(x= longitud, y= latitud, group= 1 ,col=above),size=3) + 
-      geom_path(aes(long,lat,group=group,fill=hole),color="black",size=0.3)  + 
+      geom_path(aes(long,lat,group=group),color="black",size=0.3)  + 
       theme_bw() + scale_color_gradientn(colours =myPalette2(10), limits=c(0,100)) + coord_equal() + 
       labs(title="Above", x=" ", y=" ", col=" ")+ theme(legend.position = "bottom",legend.key.width  = unit(1.5, "cm"))
     
     normal <-ggplot(sel, aes(x=long,y=lat)) + 
       geom_polygon(aes(fill=hole,group=group),fill="snow") +  
       geom_point(data=all_ind, aes(x= longitud, y= latitud, group= 1 ,col=normal),size=3) + 
-      geom_path(aes(long,lat,group=group,fill=hole),color="black",size=0.3)  + 
+      geom_path(aes(long,lat,group=group),color="black",size=0.3)  + 
       theme_bw() + scale_color_gradientn(colours =myPalette2(10), limits=c(0,100)) + coord_equal() + 
       labs(title="Normal", x=" ", y=" ", col=" ")+ theme(legend.position = "bottom",legend.key.width  = unit(1.5, "cm"))
     
     below <-ggplot(sel, aes(x=long,y=lat)) + 
       geom_polygon(aes(fill=hole,group=group),fill="snow") +  
       geom_point(data=all_ind, aes(x= longitud, y= latitud, group= 1 ,col=below),size=3) + 
-      geom_path(aes(long,lat,group=group,fill=hole),color="black",size=0.3)  + 
+      geom_path(aes(long,lat,group=group),color="black",size=0.3)  + 
       theme_bw() + scale_color_gradientn(colours =myPalette2(10), limits=c(0,100)) + coord_equal() + 
       labs(title="Below", x=" ", y=" ", col=" ")+ theme(legend.position = "bottom",legend.key.width  = unit(1.5, "cm"))
     
@@ -276,7 +289,7 @@ metric_map <- function(path_metric, path_output){
     print(above,vp=viewport(layout.pos.row=1,layout.pos.col=3))
     
     dev.off()
-    cat("Mapas Probabilidades realizados...")
+    cat("Mapas Probabilidades realizados...\n")
   }
  
 
